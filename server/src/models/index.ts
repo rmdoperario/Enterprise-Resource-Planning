21515b28a6d5a64394db5eb09359bd761f2fdd88
Ferdinand John F. Dobli <fjfdobli@addu.edu.ph>
2025-01-20 15:12:52 +0800

    Fix: Database import path in index.ts and update models index

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import sequelize from './config/database';
-import * as models from './models';
+import sequelize from '../config/database';
+import './models';
 
 const app = express();
 
